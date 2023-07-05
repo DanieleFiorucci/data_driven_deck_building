823c7033fed5e2839f45ea91b53212b74162fd9b
Daniele Fiorucci <130229290+DanieleFiorucci@users.noreply.github.com>
2023-07-05 09:34:44 +0200

    Update end_page.py

--- pages/end_page.py
+++ pages/end_page.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu May  4 21:25:14 2023
-
-@author: leo
-"""
 
 import dash_html_components as html
 import dash
@@ -21,11 +16,7 @@ layout = html.Div([
                " a lot of games with your brand new deck. Don't esitate to tell"
                " to your friends about us and if you need some suggestions about"
                " the Yu-Gi-Oh game contact us, we will be glad to help you."
-               " Hope to see you soon!!!"),
-        html.H3('Our Team'),
-        html.P("Daniele 'The Rock Breaker' Fiorucci"),
-        html.P("Francesco 'Bisteccone' Migliore \n"),
-        html.P("Leonardo 'The Shepherd' Berrettoni")
+               " Hope to see you soon!!!")
         ])
 ])
 
